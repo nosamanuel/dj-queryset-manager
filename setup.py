23981dd5908b423104bdf51e6882d043b3efdc6e
Noah Seger <nosamanuel@gmail.com>
2013-10-09 00:14:33 -0500

    Include package data to fix bad install (v0.1.1)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='dj-queryset-manager',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/nosamanuel/dj-queryset-manager',
     license=open('LICENSE').read(),
     author='Noah Seger',
@@ -12,6 +12,8 @@ setup(
     description='Stop writing Django querysets.',
     long_description=open('README.rst').read(),
     py_modules=['dj_queryset_manager'],
+    package_data={'': ['LICENSE', 'README.rst']},
+    include_package_data=True,
     test_suite='tests',
     tests_require=['Django>=1.2']
 )
